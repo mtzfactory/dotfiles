909c82b1830c33f7668d82d23727bfb6c9384101
Ricardo Martinez <ricardo.martinez.monje@gmail.com>
2022-01-16 21:03:00 +0100

    Reorder zsh custom script

--- zsh/custom.zsh
+++ zsh/custom.zsh
@@ -4,20 +4,51 @@ autoload -U zmv
 # opting out homebrew analytics
 export HOMEBREW_NO_ANALYTICS=1
 
+##
 # apps
+##
+
+# android
+ANDROID="$HOME/Library/Android"
+if [ -d "$ANDROID" ]; then
+  export ANDROID_HOME="$ANDROID/sdk"
+  export ANDROID_SDK_ROOT="$ANDROID/sdk"
+  export PATH="$PATH:$ANDROID_HOME/emulator"
+  export PATH="$PATH:$ANDROID_HOME/platform-tools"
+  export PATH="$PATH:$ANDROID_HOME/tools"
+  export PATH="$PATH:$ANDROID_HOME/tools/bin"
+fi
+
+# base root directory for brew intalled apps
 USR_LOCAL_OPT="/usr/local/opt"
 
 # coreutils
 COREUTILS="$USR_LOCAL_OPT/coreutils"
-[ -d "$COREUTILS" ] && export PATH="$PATH:$COREUTILS/bin"
+[ -d "$COREUTILS" ] && export PATH="$PATH:$COREUTILS/libexec/gnubin"
+
+# binutils
+BINUTILS="$USR_LOCAL_OPT/binutils"
+if [ -d "$BINUTILS" ]; then
+  export PATH="$PATH:$BINUTILS/bin"
+  export LDFLAGS="$LDFLAGS -L$BINUTILS/lib"
+  export CPPFLAGS="$CPPFLAGS -I$BINUTILS/include"
+fi
+
+# esp-idf
+ESP_IDF="$HOME/esp/esp-idf"
+[ -d "$ESP_IDF" ] && alias get_idf=". $ESP_IDF/export.sh"
 
 # gnu-sed
 GNU_SED="$USR_LOCAL_OPT/gnu-sed"
-[ -d "$GNU_SED" ] && export PATH="$PATH:$GNU_SED/bin"
+[ -d "$GNU_SED" ] && export PATH="$PATH:$GNU_SED/libexec/gnubin"
 
 # gnu-getopt
 GNU_GETOPT="$USR_LOCAL_OPT/gnu-getopt"
 [ -d "$GNU_GETOPT" ] && export PATH="$PATH:$GNU_GETOPT/bin"
+
+# grep
+GREP="%USR_LOCAL_OPT/grep"
+[ -d "$GREP" ] && export PATH="$PATH:$GREP/libexec/gnubin"
 
 # gettext
 GETTEXT="$USR_LOCAL_OPT/gettext"
@@ -27,9 +58,42 @@ if [ -d "$GETTEXT" ]; then
   export CPPFLAGS="$CPPFLAGS -I$GETTEXT/include"
 fi
 
+# icu4c
+ICU4C="$USR_LOCAL_OPT/icu4c"
+if [ -d "$ICU4C" ]; then
+  export PATH="$PATH:$ICU4C/bin"
+  export PATH="$PATH:$ICU4C/sbin"
+  export LDFLAGS="$LDFLAGS -L$ICU4C/lib"
+  export CPPFLAGS="$CPPFLAGS -I$ICU4C/include"
+fi
+
 # libffi
 LIBFFI="$USR_LOCAL_OPT/libffi"
 [ -d "$LIBFFI" ] && export LDFLAGS="$LDFLAGS -L$LIBFFI/lib"
+
+# ncurses
+NCURSES="$USR_LOCAL_OPT/ncurses"
+if [ -d "$NCURSES" ]; then
+  export PATH="$PATH:$NCURSES/bin"
+  export LDFLAGS="$LDFLAGS -L$NCURSES/lib"
+  export CPPFLAGS="$CPPFLAGS -I$NCURSES/include"
+  export PKG_CONFIG_PATH="$NCURSES/lib/pkgconfig"
+fi
+
+# nvm
+export NVM_DIR="$HOME/.nvm"
+NVM="$USR_LOCAL_OPT/nvm"
+# This loads nvm
+[ -s "$NVM/nvm.sh" ] && . "$NVM/nvm.sh"
+# This loads nvm bash_completion
+[ -s "$NVM/etc/bash_completion.d/nvm" ] && . "$NVM/etc/bash_completion.d/nvm"
+
+# openjdk
+OPENJDK="$USR_LOCAL_OPT/openjdk"
+if [ -d "$OPENJDK" ]; then
+  export PATH="$PATH:$OPENJDK/bin"
+  export CPPFLAGS="$CPPFLAGS -I$OPENJDK/include"
+fi
 
 # openssl
 OPENSSL="$USR_LOCAL_OPT/openssl@1.1"
@@ -46,39 +110,6 @@ if [ -d "$READLINE" ]; then
   export CPPFLAGS="$CPPFLAGS -I$READLINE/include"
 fi
 
-# sqlite
-SQLITE="$USR_LOCAL_OPT/sqlite"
-if [ -d "$SQLITE" ]; then
-  export PATH="$PATH:$SQLITE/bin"
-  export LDFLAGS="$LDFLAGS -L$SQLITE/lib"
-  export CPPFLAGS="$CPPFLAGS -I$SQLITE/include"
-fi
-
-# icu4c
-ICU4C="$USR_LOCAL_OPT/icu4c"
-if [ -d "$ICU4C" ]; then
-  export PATH="$PATH:$ICU4C/bin"
-  export PATH="$PATH:$ICU4C/sbin"
-  export LDFLAGS="$LDFLAGS -L$ICU4C/lib"
-  export CPPFLAGS="$CPPFLAGS -I$ICU4C/include"
-fi
-
-# openjdk
-OPENJDK="$USR_LOCAL_OPT/openjdk"
-if [ -d "$OPENJDK" ]; then
-  export PATH="$PATH:$OPENJDK/bin"
-  export CPPFLAGS="$CPPFLAGS -I$OPENJDK/include"
-fi
-
-# ncurses
-NCURSES="$USR_LOCAL_OPT/ncurses"
-if [ -d "$NCURSES" ]; then
-  export PATH="$PATH:$NCURSES/bin"
-  export LDFLAGS="$LDFLAGS -L$NCURSES/lib"
-  export CPPFLAGS="$CPPFLAGS -I$NCURSES/include"
-  export PKG_CONFIG_PATH="$NCURSES/lib/pkgconfig"
-fi
-
 # python@3
 PYTHON3="$USR_LOCAL_OPT/python@3"
 if [ -d "$PYTHON3" ]; then
@@ -88,14 +119,6 @@ fi
 
 # python@2.7
 #export PATH="$HOME/Library/Python/2.7/bin:$PATH"
-
-# nvm
-export NVM_DIR="$HOME/.nvm"
-NVM="$USR_LOCAL_OPT/nvm"
-# This loads nvm
-[ -s "$NVM/nvm.sh" ] && . "$NVM/nvm.sh"
-# This loads nvm bash_completion
-[ -s "$NVM/etc/bash_completion.d/nvm" ] && . "$NVM/etc/bash_completion.d/nvm"
 
 # ruby
 if [ -d "$HOME/.gem" ]; then
@@ -108,40 +131,36 @@ fi
 #export CPPFLAGS="$CPPFLAGS -I$RUBY/include"
 
 # sming - esp8266
-BINUTILS="$USR_LOCAL_OPT/binutils"
-if [ -d "$BINUTILS" ]; then
-  export PATH="$PATH:$BINUTILS/bin"
-  export LDFLAGS="$LDFLAGS -L$BINUTILS/lib"
-  export CPPFLAGS="$CPPFLAGS -I$BINUTILS/include"
-fi
-
 if [ -d "$HOME/opt" ]; then
   export ESP_HOME="$HOME/opt/esp-quick-toolchain"
   export SMING_HOME="$HOME/opt/Sming/Sming"
 fi
 
-# esp-idf
-ESP_IDF="$HOME/esp/esp-idf"
-[ -d "$ESP_IDF" ] && alias get_idf=". $ESP_IDF/export.sh"
+# sqlite
+SQLITE="$USR_LOCAL_OPT/sqlite"
+if [ -d "$SQLITE" ]; then
+  export PATH="$PATH:$SQLITE/bin"
+  export LDFLAGS="$LDFLAGS -L$SQLITE/lib"
+  export CPPFLAGS="$CPPFLAGS -I$SQLITE/include"
+fi
 
-# android
-export ANDROID_HOME="$HOME/Library/Android/sdk"
-export ANDROID_SDK=$ANDROID_HOME
-export ANDROID_SDK_ROOT=$ANDROID_HOME
-export PATH="$PATH:$ANDROID_HOME/emulator"
-export PATH="$PATH:$ANDROID_HOME/platform-tools"
-export PATH="$PATH:$ANDROID_HOME/tools"
-export PATH="$PATH:$ANDROID_HOME/tools/bin"
-
+##
 # custom git commands
+##
 export PATH="$PATH:$HOME/custom-git-commands"
 
+##
 # alias
+#
 alias ls="ls -lisa"
+
+# android adb
 alias a:devices="adb devices"
 alias a:reverse="adb reverse tcp:8081 tcp:8081; adb reverse tcp:8097 tcp:8097"
 alias a:reload="adb shell input text 'RR'"
 alias a:shake="adb shell input keyevent 82"
+
+# react native - android
 alias a:task="cd ./android && ./gradlew -PversionCode=1 tasks && cd .."
 alias a:clean="cd ./android && ./gradlew -PversionCode=1 clean && cd .."
 alias a:debug="cd ./android && ./gradlew -PversionCode=1 -PversionName=\"0.0.1\" app:installDebug && cd .."
