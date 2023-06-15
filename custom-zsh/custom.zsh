3963cd1a9efd0666bc197168fc9fc40fa248b115
Ricardo Martinez <ricardo.martinez.monje@gmail.com>
2023-06-15 15:23:19 +0200

    Add alias to get global installed packages

--- custom-zsh/custom.zsh
+++ custom-zsh/custom.zsh
@@ -310,6 +310,9 @@ alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en1"
 alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
 
+# list npm or yarn global packages
+alias global-packages="yarn global list; npm list --global --depth 0"
+
 # npm package
 alias scripts="jq -r .scripts package.json"
 alias dependencies="jq -r .dependencies package.json"
