195e9195aa08493715fc4692b24cbae0b8b500db
Ricardo Martínez <ricardo.martinez.monje@gmail.com>
2024-09-06 18:20:41 +0200

    Update focused file in lvim navigation tree

--- symlinks/_config/lvim/lua/user/core.lua
+++ symlinks/_config/lvim/lua/user/core.lua
@@ -2,6 +2,11 @@ local M = {}
 
 M.config = function()
   lvim.format_on_save.enabled = true
+
+  lvim.builtin.nvimtree.setup.update_focused_file = {
+    enable = true,
+    update_cwd = true,
+  }
 end
 
 
