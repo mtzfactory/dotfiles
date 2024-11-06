cf7e80cbfa07601045ffe5087e6113b6be63dabe
Ricardo Martínez <200234+mtzfactory@users.noreply.github.com>
2024-11-06 15:20:56 +0100

    Move ls color inside if condition

--- mac/customize.zsh
+++ mac/customize.zsh
@@ -299,13 +299,6 @@ customize() {
 customize
 unset -f customize
 
-# Detect which `ls` flavor is in use
-if ls --color > /dev/null 2>&1; then # GNU `ls`
-    colorflag="--color"
-else # OS X `ls`
-    colorflag="-G"
-fi
-
 ##
 # Bindkeys
 #
@@ -323,6 +316,13 @@ bindkey -M vicmd 'j' history-substring-search-down
 if [ -x "$(command -v eza)" ]; then
   alias ls="eza --all --header --long --octal-permissions --git --icons=always --show-symlinks" 
 else
+  # Detect which `ls` flavor is in use
+  if ls --color > /dev/null 2>&1; then # GNU `ls`
+      colorflag="--color"
+  else # OS X `ls`
+      colorflag="-G"
+  fi
+
   alias ls="ls -lisa ${colorflag}"
 
   # list all files colorized in long format, including dot files
