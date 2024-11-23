84ed8f011524ccf4cb2f2441767c4565d8da5d1c
Ricardo Martínez <200234+mtzfactory@users.noreply.github.com>
2024-11-23 20:51:13 +0100

    Check if omz is installed to install plugins and theme

--- mac/install_apps.zsh
+++ mac/install_apps.zsh
@@ -227,21 +227,39 @@ if [ "$(echo $SHELL)" = "/bin/zsh" ]; then
   # Install Oh My Zsh
   sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-  declare -A ZSH_PLUGINS
+  if [[ -x $(command -v omz) ]]; then
+    # Plugin customization
+    declare -A ZSH_INSTALL_PLUGINS
 
-  ZSH_PLUGINS[zsh-autosuggestions]="https://github.com/zsh-users/zsh-autosuggestions"
-  ZSH_PLUGINS[zsh-bat]="https://github.com/fdellwing/zsh-bat.git"
-  ZSH_PLUGINS[zsh-history-substring-search]="https://github.com/zsh-users/zsh-history-substring-search"
-  ZSH_PLUGINS[zsh-syntax-highlighting]="https://github.com/zsh-users/zsh-syntax-highlighting.git "
-  ZSH_PLUGINS[zsh-z]="https://github.com/agkozak/zsh-z"
+    ZSH_INSTALL_PLUGINS[zsh-autosuggestions]="https://github.com/zsh-users/zsh-autosuggestions"
+    ZSH_INSTALL_PLUGINS[zsh-bat]="https://github.com/fdellwing/zsh-bat.git"
+    ZSH_INSTALL_PLUGINS[zsh-history-substring-search]="https://github.com/zsh-users/zsh-history-substring-search"
+    ZSH_INSTALL_PLUGINS[zsh-syntax-highlighting]="https://github.com/zsh-users/zsh-syntax-highlighting.git "
+    ZSH_INSTALL_PLUGINS[zsh-z]="https://github.com/agkozak/zsh-z"
 
-  for plugin in "${!my_dict[@]}"; do
-    git clone --depth=1 "${ZSH_PLUGINS[$key]}" "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/${key}"
-  done
+    local ZSH_INSTALL_PLUGIN
+    for ZSH_INSTALL_PLUGIN in "${!ZSH_INSTALL_PLUGINS[@]}"; do
+      git clone --depth=1 "${ZSH_INSTALL_PLUGINS[$ZSH_INSTALL_PLUGIN]}" "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/$ZSH_INSTALL_PLUGIN"
+      omz plugin enable "$ZSH_INSTALL_PLUGIN"
+    done
 
-  ## ZSH theme customization
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-  p10k configure
+    declare -a ZSH_ENALBE_PLUGINS=(
+      encode64
+      transfer
+    )
+
+    local ZSH_ENABLE_PLUGIN
+    for ZSH_ENABLE_PLUGIN in "${!ZSH_ENALBE_PLUGINS[@]}"; do
+      omz plugin enable "$ZSH_ENABLE_PLUGIN"
+    done
+
+    omz plugin enable eza
+
+    ## Theme customization
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+    omz theme set "powerlevel10k/powerlevel10k"
+    p10k configure
+  fi
 fi
 
 ## Vim plug install
