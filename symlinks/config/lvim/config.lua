6cc0fd4368b4799323f41f8c086711ad7ac9d7af
Ricardo Martínez <200234+mtzfactory@users.noreply.github.com>
2025-02-01 17:58:48 +0100

    Reorder lvim config file

--- symlinks/config/lvim/config.lua
+++ symlinks/config/lvim/config.lua
@@ -3,10 +3,11 @@
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
+require("user.core").config()
+require("user.dap").config()
+require("user.bufferline").config()
+require("user.keybindings").config()
 require("user.null_ls").config()
+require("user.nvim-lspconfig").config()
 require("user.nvim-tree").config()
 require("user.plugins").config()
-require("user.nvim-lspconfig").config()
-require("user.keybindings").config()
-require("user.bufferline").config()
-require("user.core").config()
