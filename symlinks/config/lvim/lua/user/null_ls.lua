8cc7627581b7efaa7dfc7c031cdf35003b293d0f
Ricardo Martínez <200234+mtzfactory@users.noreply.github.com>
2025-02-13 21:14:52 +0100

    Change null_ls config causing problems for eslint/prettier

--- symlinks/config/lvim/lua/user/null_ls.lua
+++ symlinks/config/lvim/lua/user/null_ls.lua
@@ -1,61 +1,32 @@
 local M = {}
 
--- Install plugins using Mason --> <leader>l I
+-- Install plugins using Mason --> <leader> l I
 
 M.config = function()
   lvim.lsp.null_ls.setup.debug = true
 
-  local condition_eslintd = function(utils)
-    return utils.root_has_file({
-      ".eslintrc",
-      ".eslintrc.js",
-      ".eslintrc.cjs",
-      ".eslintrc.yaml",
-      ".eslintrc.yml",
-      ".eslintrc.json",
-      "eslint.config.js",
-      "eslint.config.mjs",
-      "eslint.config.json"
-    })
-  end
-
-  local condition_prettierd = function(utils)
-    return utils.root_has_file({
-      ".prettierrc",
-      ".prettierrc.json",
-      ".prettierrc.yml",
-      ".prettierrc.yaml",
-      ".prettierrc.json5",
-      ".prettierrc.js",
-      ".prettierrc.cjs",
-      ".prettierrc.toml",
-      "prettier.config.js",
-      "prettier.config.cjs"
-    })
-  end
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx"
+  }
 
   local linters = require "lvim.lsp.null-ls.linters"
   linters.setup {
-    {
-      command = "eslint_d",
-      condition = condition_eslintd
-    }
+    { command = "eslint_d", filetypes = filetypes }
   }
 
   local formatters = require "lvim.lsp.null-ls.formatters"
   formatters.setup {
-    {
-      command = "eslint_d",
-      condition = condition_eslintd,
-    },
-    {
-      command = "prettierd",
-      condition = condition_prettierd
-    }
+    { command = "eslint_d",  filetypes = filetypes },
+    { command = "prettierd", filetypes = filetypes }
   }
 
   local code_actions = require "lvim.lsp.null-ls.code_actions"
-  code_actions.setup { { command = "eslint_d" } }
+  code_actions.setup { { command = "eslint_d", filetypes = filetypes } }
 
   local null_ls = require("null-ls")
 
