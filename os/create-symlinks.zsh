85ee6105ad8ffe36a291805af4690da9d828d18b
Ricardo Martínez <200234+mtzfactory@users.noreply.github.com>
2026-06-10 22:12:44 +0200

    add gitconfig for home-lab

--- os/create-symlinks.zsh
+++ os/create-symlinks.zsh
@@ -9,6 +9,7 @@ local DOTFILES_SYMLINKS="$DOTFILES/symlinks"
 # Git configs
 declare -a GIT_CONFIGS=(
   "gitconfig"
+  "gitconfig-home-lab"
   "gitconfig-personal"
   "gitconfig-work"
   "gitignore-global"
